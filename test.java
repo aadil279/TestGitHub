981255490433f11db1de5d2c318ac2ddae1434d7
aadil279 <aimsk@iscte-iul.pt>
2021-12-13 18:57:05 +0000

    Update test.java
    Added user input: if 1 -> Hello World

--- test.java
+++ test.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class test {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Scanner s = new Scanner(System.in);
+		int ans = s.nextInt();
+		while(true) {
+			if(ans==1) {
+				System.out.println("Hello World!");
+			}
+		}
 	}
 }
